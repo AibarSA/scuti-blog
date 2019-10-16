e47de3d54210c9bf035dd37b49c4327723351286
Aibar-B <aibro_85@mail.ru>
2019-10-16 19:31:10 +0600

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,10 +9,9 @@ const CollectionPreview = ({title, items}) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items
-                .filter((item, idx) => idx < 4)
-                .map(({id, name, price, imageUrl}) => (
-                    <CollectionItem key={id} name={name} price={price} imageUrl={imageUrl}/>
+                items.filter((item, idx) => idx < 4)
+                .map((item) => (
+                    <CollectionItem key={item.id} item={item}/>
                 ))
             }
 
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, isGoogleSignIn, handleChange,googleSignIn, label, name, type, value, required}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} onChange={handleChange} name={name}
-    type={type} value={value} required={required} onClick={googleSignIn}>
+const CustomButton = ({children, isGoogleSignIn, handleChange,googleSignIn,inverted,addItemToCart, label, name, type, value, required}) => (
+    <button className={
+        `${inverted ? 'inverted' : ''} 
+        ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} onChange={handleChange} name={name}
+    type={type} value={value} required={required} onClick={googleSignIn, addItemToCart}>
         {children}
     </button>
 )
