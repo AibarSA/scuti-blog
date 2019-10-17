65ab329b6b5378b6601d6726e8fd529dc4a0d56c
Aibar-B <aibro_85@mail.ru>
2019-10-17 18:38:36 +0600

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, isGoogleSignIn, handleChange,googleSignIn,inverted,addItemToCart, label, name, type, value, required}) => (
+const CustomButton = ({children, isGoogleSignIn, handleChange,googleSignIn,goToCheckout,inverted,addItemToCart, label, name, type, value, required}) => (
     <button className={
         `${inverted ? 'inverted' : ''} 
         ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} onChange={handleChange} name={name}
-    type={type} value={value} required={required} onClick={googleSignIn ? googleSignIn : addItemToCart}>
+    type={type} value={value} required={required} onClick={ 
+        googleSignIn ? googleSignIn : goToCheckout ? goToCheckout :  addItemToCart}>
         {children}
     </button>
 )
